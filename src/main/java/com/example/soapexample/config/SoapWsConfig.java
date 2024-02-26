989a1426b3f81355caf7e7699e52f87cafb42f0b
Collins Bett <collinsbett24@gmail.com>
2024-02-26 23:01:28 +0300

    Create xsd file and WS config

---
+++ src/main/java/com/example/soapexample/config/SoapWsConfig.java
@@ -0,0 +1,39 @@
+package com.example.soapexample.config;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+
+@Configuration
+@EnableWs
+public class SoapWsConfig {
+
+    public ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServletServletRegistrationBean(ApplicationContext context){
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(context);
+        servlet.setTransformSchemaLocations(true);
+        return  new ServletRegistrationBean<MessageDispatcherServlet>(servlet, "/ws/*");
+    }
+
+    @Bean(name = "account_balance")
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema schema){
+        DefaultWsdl11Definition defaultWsdl11Definition = new DefaultWsdl11Definition();
+        defaultWsdl11Definition.setPortTypeName("accountBalanceIndicator");
+        defaultWsdl11Definition.setLocationUri("/ws");
+        defaultWsdl11Definition.setTargetNamespace("");
+        defaultWsdl11Definition.setSchema(schema);
+        return  defaultWsdl11Definition;
+    }
+
+    @Bean
+    public  XsdSchema schema(){
+        return new SimpleXsdSchema(new ClassPathResource("get-status-request.xsd"));
+    }
+}
